edddce55948d01a2a1cc0c8d83aa3141eed62d49
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-10-16 21:18:42 +0300

    done

--- routes/api/contacts.router.js
+++ routes/api/contacts.router.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const contactsControllers = require('../../controllers');
-const validationMiddleware = require('../../middlewares/validationMiddleware');
-const contactsSchema = require('../../schemas/contactsSchemas');
 
 router.get('/', contactsControllers.getAll);
 
 router.get('/:contactId', contactsControllers.getById);
 
-router.post('/', validationMiddleware(contactsSchema), contactsControllers.add);
+router.post('/', contactsControllers.add);
 
 router.delete('/:contactId', contactsControllers.removeById);
 
 router.put(
   '/:contactId',
-  validationMiddleware(contactsSchema),
   contactsControllers.updateById
 );
 
